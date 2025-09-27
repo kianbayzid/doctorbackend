e7ae49c9ed864dfa27e1744fe91f9092bd72212b
kevin sturge <crazyguy644@gmail.com>
2025-09-27 15:31:16 -0400

    moslty finished messages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import doctorsRouter from './doctors.router.js';
 import patientsRouter from './patients.router.js';
+import messagesRouter from './message.router.js';
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/doctors', doctorsRouter);
   router.use('/patients', patientsRouter);
+  router.use('/messages', messagesRouter);
 }
 
 export default routerApi;
